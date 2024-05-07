079311582acede40b13f08afd5c7bae938f609af
Sama Rahimian <{ID}+{username}@users.noreply.github.com>
2024-05-06 23:16:35 -0400

    Minor tweaks to the license

--- huh/__init__.py
+++ huh/__init__.py
@@ -13,15 +13,19 @@ __license__ = 'MPL-2.0'
 
 
 """ 
-    huh: huququ'llah helper Copyright (C) 2024  Sama Rahimian
+    huh: huququ'llah helper 
+    Copyright (C) 2024, Sama Rahimian. All rights reserved.
 
-    This Source Code Form is subject to the terms of the Mozilla Public
-    License, v. 2.0. If a copy of the MPL was not distributed with this
-    file, You can obtain one at http://mozilla.org/MPL/2.0/.
+    This Source Code Form is subject to the terms of the Mozilla
+    Public License, v. 2.0. If a copy of the MPL was not distributed
+    with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
     This program is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. In no world,
+    this or the next, shall the Author be held liable for any
+    damages, loss, damnation, or miscalculations, arising from the use
+    of this software.
 
 """
 
